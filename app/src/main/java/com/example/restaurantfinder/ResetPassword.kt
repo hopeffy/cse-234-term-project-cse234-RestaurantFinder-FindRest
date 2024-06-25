bf8d44a4b7ccf765490f9c6779381719bc23fe3c
Eftelya <20200808026@ogr.akdeniz.edu.tr>
2024-06-25 20:12:51 +0300

    forget password added
    send a mail to reset the password and apply new password

--- app/src/main/java/com/example/restaurantfinder/ResetPassword.kt
+++ app/src/main/java/com/example/restaurantfinder/ResetPassword.kt
@@ -1,6 +1,7 @@
 package com.example.restaurantfinder
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,24 +18,57 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.restaurantfinder.data.Account
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import org.jetbrains.annotations.ApiStatus.Experimental
 
 
 @Composable
 fun ResetPassword(navController: NavHostController) {
+    val auth = FirebaseAuth.getInstance()
+    val database = FirebaseDatabase.getInstance().getReference("accounts")
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
+    var errorMessage by remember { mutableStateOf("") }
 
     var email by remember { mutableStateOf("") }
+
+    SideEffect {
+
+        if (email.isNotEmpty()) {
+            coroutineScope.launch(IO) {
+                try {
+                    auth.sendPasswordResetEmail(email).await()
+
+                    errorMessage = "A password reset link has been sent to your email"
+                } catch (e: FirebaseAuthException) {
+                    errorMessage = "Failed to send password reset email: ${e.message}"
+                }
+            }
+        }
+    }
+
 
     Column(
         modifier = Modifier
@@ -68,6 +102,8 @@ fun ResetPassword(navController: NavHostController) {
                     label = { Text(text = "E-mail") },
                     modifier = Modifier.fillMaxWidth()
                 )
+
+
                 Spacer(modifier = Modifier.height(6.dp))
                 Text(
                     text = "Enter your e-mail address",
